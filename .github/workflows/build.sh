7cda1145f3f691bd510ad07d72f712ffa78272db
Lucien Shui <lucien@lucien.ink>
2024-05-06 01:27:17 +0800

    Add favicon support

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -13,9 +13,31 @@ for i in range(len(lines)):
         lines[i] = '    <p>' + lines[i] + '</p>'
 result = '<div id=\"intro\">\n    <h1>Intro</h1>\n' + '\n'.join(lines) + '\n</div>'
 print(result)"
+PY_HEAD_CODE="import sys
+content = ''.join(sys.stdin.readlines())
+lines = content.strip().split('\n')
+src = '<meta charset=\"UTF-8\"/>'
+dst = \"\"\"<meta charset=\"UTF-8\"/>
+    <meta name=\"apple-mobile-web-app-capable\" content=\"yes\">
+    <meta name=\"apple-mobile-web-app-status-bar-style\" content=\"black-translucent\">
+    <link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/assets/img/favicons/apple-touch-icon.png\">
+    <link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/assets/img/favicons/favicon-32x32.png\">
+    <link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/assets/img/favicons/favicon-16x16.png\">
+    <link rel=\"manifest\" href=\"/assets/img/favicons/site.webmanifest\">
+    <link rel=\"shortcut icon\" href=\"/assets/img/favicons/favicon.ico\">
+    <meta name=\"apple-mobile-web-app-title\" content=\"I am Lucien\">
+    <meta name=\"application-name\" content=\"I am Lucien\">
+    <meta name=\"msapplication-TileColor\" content=\"#da532c\">
+    <meta name=\"msapplication-config\" content=\"/assets/img/favicons/browserconfig.xml\">
+    <meta name=\"theme-color\" content=\"#ffffff\">\"\"\"
+print(content.replace(src, dst))
+"
 
 wget -O - "${DOWNLOAD_URL}" | tar -xzvf - && \
 mv i-am-lucien _site && \
 yes | rm _site/page/*example.* && \
 cp page/* _site/page/ && \
-curl -sSL 'https://raw.githubusercontent.com/LucienShui/epitaph/main/README.md' | python3 -c "${PY_CODE}" > _site/page/intro.html
+curl -sSL 'https://raw.githubusercontent.com/LucienShui/epitaph/main/README.md' | python3 -c "${PY_CODE}" > _site/page/intro.html && \
+python3 -c "${PY_HEAD_CODE}" < _site/index.html > tmp && \
+mv tmp _site/index.html && \
+cp -r assets/img _site/assets/
